c966051572bf4007d61c7e61e77f7c7df810798c
echandsome <echandsome@hotmail.com>
2024-11-04 08:13:05 -0800

    Signed-off-by: echandsome <echandsome@hotmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Chat from './components/Chat/Chat';
 import Join from './components/Join/Join';
-
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   return (
--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import onlineIcon from '../../icons/onlineIcon.png';
 import closeIcon from '../../icons/closeIcon.png';
+
 import './InfoBar.css';
 
 const InfoBar = ({ room }) => (
